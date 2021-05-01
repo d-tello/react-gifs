94ba69afcd8e9d0a72ceef1e8869db9a25ae4b57
Davide Tello <tello.davide@gmail.com>
2021-05-01 12:21:30 +0200

    Add Gifs to the right side

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
+import giphy from "giphy-api";
+
 import SearchBar from "./search_bar";
 import Gif from "./gif";
-
+import GifList from "./gif_list";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGifId: "3Owa0TWYqHi5RZYGql",
+    };
+
+    this.search("Shiba");
+  }
+
+  search = (query) => {
+    giphy("4qibyMTNmgEyvdCA004HJ2kLEK1lWan8").search(
+      {
+        q: query,
+        rating: "g",
+      },
+      (error, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      }
+    );
+  };
+
   render() {
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
       </div>
     );
   }
